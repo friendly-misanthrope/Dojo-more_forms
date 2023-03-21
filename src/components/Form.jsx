5445f47974139502e3bfce8ee6df1a6633942240
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-21 00:00:57 -0700

    added validation messages for all inputs, changed submit btn to be disabled if form data isn't valid. Added functionality to map through userList array and display each user's data below the form if any users have been submitted.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -50,7 +50,12 @@ const Form = (props) => {
       setUserList(prevUserList => {return [...prevUserList, user]})
 
       // Reset user object to default value
-      setUser()
+      setUser(prevUserState => {return {...prevUserState,
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''}})
 
       // Set hasBeenSubmitted to true
       setHasBeenSubmitted(true)
@@ -63,43 +68,88 @@ const Form = (props) => {
   return (
     <div>
       <h1>Hook Form</h1>
+      {/* Display form message depending on whether form has been submitted */}
       <h2>{formMessage()}</h2>
       <div className="user-form">
-      
+        {/* Call submitUser() when form is submitted */}
         <form onSubmit={submitUser}>
           <div className="form-group">
             <label htmlFor="firstName">First Name: </label>
             <input type="text" className="form-control" id="firstName" name="firstName" onChange={handleUserChange} value={firstName} />
           </div>
 
+          {/* Conditionally render validation messages underneath input fields if data isn't valid */}
+          {
+            firstName.length < 2 && firstName.length > 0 ?
+              <p className="errorMsg">First Name must be at least 2 characters</p>
+              : null
+          }
+
           <div className="form-group">
             <label htmlFor="lastName">Last Name: </label>
             <input type="text" className="form-control" id="lastName" name="lastName" onChange={handleUserChange} value={lastName} />
           </div>
+          {
+            lastName.length < 2 && lastName.length > 0 ?
+              <p className="errorMsg">Last Name must be at least 2 characters</p>
+              : null
+          }
 
           <div className="form-group">
             <label htmlFor="email">Email: </label>
             <input type="text" className="form-control" id="email" name="email" onChange={handleUserChange} value={email} />
           </div>
+          {
+            email.length < 5 && email.length > 0 ?
+              <p className="errorMsg">Email must be at least 5 characters</p>
+              : null
+          }
 
           <div className="form-group">
             <label htmlFor="password">Password: </label>
             <input type="password" className="form-control" id="password" name="password" onChange={handleUserChange} value={password} />
           </div>
+          {
+            password.length < 8 && password.length > 0 ?
+              <p className="errorMsg">Password must be at least 8 characters</p>
+              : null
+          }
 
           <div className="form-group">
             <label htmlFor="confirmPassword">Confirm Password: </label>
             <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" onChange={handleUserChange} value={confirmPassword} />
           </div>
+          {
+            confirmPassword.length > 0 && confirmPassword !== password ?
+              <p className="errorMsg">Passwords must match</p>
+              : null
+          }
 
           {
-            validateForm() ?
-            <input type="submit" value="Submit" />
-            : <input type="submit" value="Submit" disabled />
+            <input className="btn btn-primary" type="submit" value="submit" disabled={validateForm() ? false :true} />
           }
 
         </form>
       </div>
+
+      {/* If there are users in userList, map through the array and display each one */}
+      {
+        userList.length > 0 ?
+          <div className="allUsers">
+            <h2>All Users</h2>
+            {
+              userList.map((user, index) => (
+                <div key={index} className="userCard">
+                  <h4>{user.firstName} {user.lastName}</h4>
+                  <p>Email: {user.email}</p>
+                  <p>Password: {user.password}</p>
+                  <p>Password Confirmation: {user.confirmPassword}</p>
+                </div>
+              ))
+            }
+          </div>
+          : null
+      }
 
     </div>
   );
